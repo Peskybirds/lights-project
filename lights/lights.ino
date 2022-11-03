2dbcd80af5eba1ab63bd6343dd05b1fb8f1e1547
Peskybirds <116131241+Peskybirds@users.noreply.github.com>
2022-11-03 14:26:59 +0000

    lightV2

---
+++ lights/lights.ino
@@ -0,0 +1,15 @@
+
+#include "pinGroup.h"
+void setup() {
+  // put your setup code here, to run once:
+pinMode(2, INPUT);
+int sensorPin = A1;
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(ledPin, HIGH)
+delay(100)
+digitalWrite(ledPin, LOW)
+}
