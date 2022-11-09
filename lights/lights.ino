d54122601c2b54cec3a6667cf932b37954bdcbe9
SamBerragan <117365804+SamBerragan@users.noreply.github.com>
2022-11-09 14:22:14 +0000

    Update lights.ino

--- lights/lights.ino
+++ lights/lights.ino
@@ -1,32 +1,47 @@
-
-#include "pinGroup.h"
+int readValue = 0;
 void setup() {
   // put your setup code here, to run once:
-pinMode(2, INPUT);
-pinMode(3, OUTPUT);
-pinMode(4, OUTPUT);
-pinMode(5, OUTPUT);
-pinMode(6, OUTPUT);
-pinMode(7, OUTPUT);
-pinMode(8, OUTPUT);
-
-
+pinMode(2,INPUT);
+pinMode(3,OUTPUT);
+pinMode(4,OUTPUT);
+pinMode(5,OUTPUT);
+pinMode(6,OUTPUT);
+pinMode(7,OUTPUT);
+pinMode(8,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-digitalWrite(3, HIGH)
-digitalWrite(4, HIGH)
-digitalWrite(5, HIGH)
-digitalWrite(6, HIGH)
-digitalWrite(7, HIGH)
-digitalWrite(8, HIGH)
-delay(100)
-digitalWrite(3, LOW)
-digitalWrite(4, LOW)
-digitalWrite(5, LOW)
-digitalWrite(6, LOW)
-digitalWrite(7, LOW)
-digitalWrite(8, LOW)
-delay(100)
+digitalWrite(3,HIGH);
+digitalWrite(4,LOW);
+digitalWrite(5,HIGH);
+digitalWrite(6,LOW);
+digitalWrite(7,HIGH);
+digitalWrite(8,LOW);
+delay(1000);
+digitalWrite(3,LOW);
+digitalWrite(4,HIGH);
+digitalWrite(5,LOW);
+digitalWrite(6,HIGH);
+digitalWrite(7,LOW);
+digitalWrite(8,HIGH);
+if(digitalRead(2)==HIGH){
+  readValue = analogRead(A0);
+  digitalWrite(3,HIGH);
+  digitalWrite(4,HIGH);
+  digitalWrite(5,HIGH);
+  digitalWrite(6,HIGH);
+  digitalWrite(7,HIGH);
+  digitalWrite(8,HIGH);
+  delay(readValue);
+  digitalWrite(3,LOW);
+  digitalWrite(4,LOW);
+  digitalWrite(5,LOW);
+  digitalWrite(6,LOW);
+  digitalWrite(7,LOW);
+  digitalWrite(8,LOW);
+  delay(readValue);
 }
+delay(1000);
+}
+
