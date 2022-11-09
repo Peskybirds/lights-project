0651f0d0258ce33e3193bfdc8b974930eb1e1d6e
Peskybirds <116131241+Peskybirds@users.noreply.github.com>
2022-11-09 22:53:47 +0000

    Update lights.ino

--- lights/lights.ino
+++ lights/lights.ino
@@ -1,6 +1,8 @@
 // im sorry for who has to read this code i don't like indents so yea and a very quick overview on what this is meant to do
 // its meant to flash some pins on and some off and the button does math so it switches mode
 int readValue = 0;
+int number = 1;
+int rannumber = 0;
 void setup() {
   // setting all the pins as input or output
 pinMode(2,INPUT);
@@ -10,26 +12,32 @@ pinMode(5,OUTPUT);
 pinMode(6,OUTPUT);
 pinMode(7,OUTPUT);
 pinMode(8,OUTPUT);
-}
 
+}
 void loop() {
-  // flipping between 3 lights off and on
-digitalWrite(3,HIGH);
-digitalWrite(4,LOW);
-digitalWrite(5,HIGH);
-digitalWrite(6,LOW);
-digitalWrite(7,HIGH);
-digitalWrite(8,LOW);
-delay(1000);
-digitalWrite(3,LOW);
-digitalWrite(4,HIGH);
-digitalWrite(5,LOW);
-digitalWrite(6,HIGH);
-digitalWrite(7,LOW);
-digitalWrite(8,HIGH);
-// setting the delay to what analogRead is at
 if(digitalRead(2)==HIGH){
-  readValue = analogRead(A0);
+number += 1;
+}
+// if button is pressed add 1 onto number
+if(number == (1)){
+// flipping between 3 lights off and on
+  digitalWrite(3,HIGH);
+  digitalWrite(4,LOW);
+  digitalWrite(5,HIGH);
+  digitalWrite(6,LOW);
+  digitalWrite(7,HIGH);
+  digitalWrite(8,LOW);
+  delay(1000);
+  digitalWrite(3,LOW);
+  digitalWrite(4,HIGH);
+  digitalWrite(5,LOW);
+  digitalWrite(6,HIGH);
+  digitalWrite(7,LOW);
+  digitalWrite(8,HIGH);
+    }
+else if(number == (2)){
+ // setting the delay to what analogRead is at
+ readValue = analogRead(A0);
   digitalWrite(3,HIGH);
   digitalWrite(4,HIGH);
   digitalWrite(5,HIGH);
@@ -45,5 +53,25 @@ if(digitalRead(2)==HIGH){
   digitalWrite(8,LOW);
   delay(readValue);
 }
-delay(1000);
+else if(number == (3)){
+  rannumber = random(10, 1000)
+ digitalWrite(3,LOW);
+  digitalWrite(4,HIGH);
+  digitalWrite(5,LOW);
+  digitalWrite(6,HIGH);
+  digitalWrite(7,LOW);
+  digitalWrite(8,HIGH);
+  delay(rannumber);
+  digitalWrite(3,HIGH);
+  digitalWrite(4,LOW);
+  digitalWrite(5,HIGH);
+  digitalWrite(6,LOW);
+  digitalWrite(7,HIGH);
+  digitalWrite(8,LOW);
+  delay(rannumber);
+
+}
+else(number == (4));{
+number = 1;
+}
 }
