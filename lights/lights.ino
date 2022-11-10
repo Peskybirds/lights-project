2ccb3565694eb2d5a02a21d2b049b6c90fdd1048
SamBerragan <117365804+SamBerragan@users.noreply.github.com>
2022-11-10 14:34:01 +0000

    Update lights.ino

--- lights/lights.ino
+++ lights/lights.ino
@@ -3,6 +3,7 @@
 int readValue = 0;
 int number = 1;
 int rannumber = 0;
+
 void setup() {
   // setting all the pins as input or output
 pinMode(2,INPUT);
@@ -12,12 +13,14 @@ pinMode(5,OUTPUT);
 pinMode(6,OUTPUT);
 pinMode(7,OUTPUT);
 pinMode(8,OUTPUT);
+Serial.begin(9600);
 
 }
 void loop() {
-if(digitalRead(2)==HIGH){
-number += 1;
-}
+Serial.print("HELP");
+Serial.println(number);
+ 
+
 // if button is pressed add 1 onto number
 if(number == (1)){
 // flipping between 3 lights off and on
@@ -27,17 +30,31 @@ if(number == (1)){
   digitalWrite(6,LOW);
   digitalWrite(7,HIGH);
   digitalWrite(8,LOW);
-  delay(1000);
+  delay(100);
   digitalWrite(3,LOW);
   digitalWrite(4,HIGH);
   digitalWrite(5,LOW);
   digitalWrite(6,HIGH);
   digitalWrite(7,LOW);
   digitalWrite(8,HIGH);
+  delay(100);
+  if(digitalRead(2)==HIGH) {
+    Serial.print("HELLO");
+    Serial.print(" : ");
+    Serial.println(number);
+    delay(500);
+    number += 1;
+    Serial.println("ADDED");
+   while(digitalRead(2)==HIGH){
+    
+  }
+  }
     }
 else if(number == (2)){
+  Serial.println("2 called");
  // setting the delay to what analogRead is at
  readValue = analogRead(A0);
+ Serial.println(readValue);
   digitalWrite(3,HIGH);
   digitalWrite(4,HIGH);
   digitalWrite(5,HIGH);
@@ -52,8 +69,17 @@ else if(number == (2)){
   digitalWrite(7,LOW);
   digitalWrite(8,LOW);
   delay(readValue);
+  if(digitalRead(2)==HIGH) {
+  delay(500);
+    number += 1;
+  while(digitalRead(2)==HIGH){
+    
 }
+  }
+}
+
 else if(number == (3)){
+  Serial.println("3 called");
   rannumber = random(10, 1000);
  digitalWrite(3,LOW);
   digitalWrite(4,HIGH);
@@ -69,9 +95,17 @@ else if(number == (3)){
   digitalWrite(7,HIGH);
   digitalWrite(8,LOW);
   delay(rannumber);
+  if(digitalRead(2)==HIGH) {
+    delay(500);
+    number += 1;
+  while(digitalRead(2)==HIGH){
+  
+}
+  }
 
 }
-else(number == (4));{
+ else if(number >= (4)){
+  Serial.println("4 called");
 number = 1;
 }
 }
